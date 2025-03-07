7bcda6f96a8e3cb337e12b6b7ba20d8442148de4
Dharmesh Bangara <62675598+dharmeshbangaraa@users.noreply.github.com>
2025-03-07 19:29:51 +0000

    configured coverage report

--- biking/vitest.config.ts
+++ biking/vitest.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        globals: true,   
+        globals: true,
         environment: "jsdom",
-        setupFiles: "./src/setupTests.ts"
+        setupFiles: "./src/setupTests.ts",
+        coverage: {
+            reporter: ['lcov', 'text'],
+            reportsDirectory: './coverage',
+        },
     },
 });
