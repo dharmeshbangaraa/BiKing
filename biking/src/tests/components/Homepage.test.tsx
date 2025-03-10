bdc8ef6301107dd14577628b3936bf6a10dfed0b
Dharmesh Bangara <62675598+dharmeshbangaraa@users.noreply.github.com>
2025-03-10 12:32:12 +0000

    added tests for homepage components

--- biking/src/tests/components/Homepage.test.tsx
+++ biking/src/tests/components/Homepage.test.tsx
@@ -4,9 +4,28 @@ import Carousel from '../../components/carousel/HomepageCarousel';
 import Searchbar from '../../components/searchbar/Searchbar';
 import FeaturedBike from "../../components/featured bikes/FeaturedBikes";
 import GoGreen from "../../components/banners/GoGreen";
+import Homepage from "../../components/homepage/Homepage";
 
 
 describe('test should render every component of homepage successfully', () => {
+    it('test should render homepage component successfully', () => {
+        render(<Homepage />)
+
+        expect(screen.getByRole('banner')).toBeInTheDocument();
+        expect(screen.getByText("Let's search your DREAM bike together!"));
+        expect(screen.getAllByRole('textbox')).toHaveLength(2);
+        expect(screen.getByText('Featured Bikes')).toBeInTheDocument();
+        expect(screen.getByText('Browse By')).toBeInTheDocument();
+        expect(screen.getByText('Explore wide range of electric Bikes and Scooters!')).toBeInTheDocument();
+        expect(screen.getByText('Popular Scooters')).toBeInTheDocument();
+        expect(screen.getByText('Popular Brands')).toBeInTheDocument();
+        expect(screen.getByText('Trending Bikes')).toBeInTheDocument();
+        expect(screen.getByText('Upcoming Bikes')).toBeInTheDocument();
+        expect(screen.getAllByRole('tab')).toHaveLength(9);
+        expect(screen.getAllByRole('button', {name: "check out"}).length).toBeGreaterThan(0);
+
+    });
+
     it('test should render carousel successfully', () => {
         render(<Carousel />)
 
