da42a8b97d60d3bb50c901b90db21e560b967335
dharmeshbangaraa <dharmeshbangara@gmail.com>
2025-03-27 01:39:30 +0530

    fixed deployement issue

--- biking/src/components/bikes under 50k/BikesUnder50k.tsx
+++ biking/src/components/bikes under 50k/BikesUnder50k.tsx
@@ -3,28 +3,7 @@ import React, { useEffect, useState } from "react";
 import Bike from "../../interfaces/Bike";
 
 const BikesUnder50k: React.FC = () => {
-
-  const [bikes, setBikes] = useState<Bike[]>([{
-    id: 1,
-    name: "Royal Enfield Himalayan",
-    price: "3,00,000",
-    category: "Adventure",
-    image: "/images/bikes/himalayan.jpg"
-  },
-  {
-    id: 2,
-    name: "Kawasaki Ninja 300",
-    price: "3,40,000",
-    category: "Sport",
-    image: "/images/bikes/Ninja300.jpg"
-  },
-  {
-    id: 3,
-    name: "KTM RC 390",
-    price: "3,40,000",
-    category: "Sport",
-    image: "/images/bikes/RC390.jpg"
-  }]);
+  const [bikes, setBikes] = useState<Bike[]>([]);
 
   useEffect(() => {
     fetch("https://api.example.com/bikes") // Replace with real API
@@ -37,7 +16,11 @@ const BikesUnder50k: React.FC = () => {
     <div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {bikes?.map((filteredBike) => (
-          <Card key={filteredBike.id} className="hover:shadow-lg" sx={{ borderRadius: 5, borderWidth: 1, borderColor: "gray" }}>
+          <Card
+            key={filteredBike.id}
+            className="hover:shadow-lg"
+            sx={{ borderRadius: 5, borderWidth: 1, borderColor: "gray" }}
+          >
             <img
               src={filteredBike.image}
               alt={filteredBike.name}
@@ -45,9 +28,17 @@ const BikesUnder50k: React.FC = () => {
             />
             <CardContent className="bg-gray-50 p-4">
               <h3 className="text-md font-semibold">{filteredBike.name}</h3>
-              <p className="text-md font-semibold flex">&#8377; {filteredBike.price} <span className="pl-1">Onwards</span></p>
+              <p className="text-md font-semibold flex">
+                &#8377; {filteredBike.price}{" "}
+                <span className="pl-1">Onwards</span>
+              </p>
               <span className="text-sm mb-2">Avg. Ex-Showroom price</span>
-              <Button variant="text" color="primary" fullWidth sx={{ border: 1, marginTop: 2, borderRadius: 2 }}>
+              <Button
+                variant="text"
+                color="primary"
+                fullWidth
+                sx={{ border: 1, marginTop: 2, borderRadius: 2 }}
+              >
                 check out
               </Button>
             </CardContent>
@@ -55,7 +46,7 @@ const BikesUnder50k: React.FC = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default BikesUnder50k;
