d9ac9a3ac622e907453c48b45c75cc12e94d7651
dharmeshbangaraa <dharmeshbangara@gmail.com>
2025-04-08 23:19:08 +0530

    removed unused variable

--- biking/src/components/bike details/UserReview.tsx
+++ biking/src/components/bike details/UserReview.tsx
@@ -7,7 +7,6 @@ const UserReview: React.FC<
   BikeReview & { toggleExpand?: (id: number) => void }
 > = ({
   id,
-  bikeName,
   styleAndDesign,
   reliability,
   comfort,
@@ -22,40 +21,6 @@ const UserReview: React.FC<
   expanded,
   toggleExpand,
 }) => {
-  //   {
-  //     userId: 1,
-  //     name: "Dharmesh Bangara",
-  //     when: "1 week ago",
-  //     mileageReceived: 40,
-  //     kmsRidden: 500,
-  //     heading: "Royal Enfield Himalayan 450: A Practical Machine",
-  //     body: "I living in Banglore and bought this new bike and this is good in all among royal stores. A great bike to experience a real royal look. The overall experience was good but the only trouble happened at a store where my name was printed wrong, someone else name was printed while giving for servicing, I'm feeling bad that my name was not there. But overall it was still a good experience with a bike.",
-  //     styleAndDesignRating: 4.5,
-  //     ReliabilityRating: 4,
-  //     ComfortRating: 4,
-  //     ServiceExperienceRating: 4,
-  //     ValueforMoneyRating: 4,
-  //     PerformanceRating: 4,
-  //     expanded: false,
-  //   },
-  //   {
-  //     userId: 2,
-  //     name: "John Doe",
-  //     when: "1 week ago",
-  //     mileageReceived: 38,
-  //     kmsRidden: 2000,
-  //     heading: "Practical adventure tourer",
-  //     body: "The bike stands out in many aspects such as looks, practicality, handling, and capability and when compared to its competitors it's way more affordable. Based on my experience the low-end torque and throttle response is best. MINOR CONS: 1. Missed out with traction control. 2. Feels a bit heavier in traffic conditions.",
-  //     styleAndDesignRating: 4.5,
-  //     ReliabilityRating: 4,
-  //     ComfortRating: 4,
-  //     ServiceExperienceRating: 4,
-  //     ValueforMoneyRating: 4,
-  //     PerformanceRating: 4,
-  //     expanded: false,
-  //   },
-  // ]);
-
   const userReviewParams: { name: string; value: number }[] = [
     { name: "Style & Design", value: styleAndDesign },
     { name: "Reliability", value: reliability },
