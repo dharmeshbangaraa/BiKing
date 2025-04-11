08f17e2677d0813549d3b9da20b79f9058950c7c
dharmeshbangaraa <dharmeshbangara@gmail.com>
2025-04-11 20:35:57 +0530

    fixed brand description

--- biking/src/components/bike list/BikeList.tsx
+++ biking/src/components/bike list/BikeList.tsx
@@ -15,10 +15,26 @@ const BikeList: React.FC = () => {
       .then((res) => res.json())
       .then((data) => {
         setBikes(data);
-        console.log(data);
       })
       .catch((err) => console.error("Error fetching bike:", err));
   }, []);
+
+  const filterByBrand = bikes?.filter((bike) => bike.brand == brand);
+  const expensiveBike = filterByBrand
+    ?.slice()
+    .sort(
+      (a, b) =>
+        Number(b?.onRoadPrice?.replace(/,/g, "") ?? 0) -
+        Number(a?.onRoadPrice?.replace(/,/g, "") ?? 0)
+    )[0];
+
+  const startingPrice = filterByBrand
+    ?.slice()
+    .sort(
+      (a, b) =>
+        Number(a?.onRoadPrice?.replace(/,/g, "") ?? 0) -
+        Number(b?.onRoadPrice?.replace(/,/g, "") ?? 0)
+    )[0];
 
   return (
     <div className="lg:px-40 px-5">
@@ -29,12 +45,13 @@ const BikeList: React.FC = () => {
       </div>
       <div>
         <p>
-          Royal Enfield bike price starts from Rs. 1,49,900. Royal Enfield
-          offers 14 new models in India with most popular bikes being Classic
-          650, Hunter 350 and Classic 350. The upcoming bikes of Royal Enfield
-          include Himalayan 750, Scrambler 450 and Flying Flea C6. Most
-          expensive Royal Enfield bike is Super Meteor 650, which is priced at
-          Rs. 3,63,900.
+          {brand} bikes price starts from Rs. {startingPrice?.onRoadPrice}.{" "}
+          {brand} offers {filterByBrand?.length} models in India with most
+          popular bikes being{" "}
+          {filterByBrand?.map((bike) => bike.name + ", ").slice(0, 2)}
+          {filterByBrand?.map((bike) => bike.name).slice(3, 4)[0]}. Most
+          expensive {brand} bike is {expensiveBike?.name}, which is priced at
+          Rs. {expensiveBike?.onRoadPrice}.
         </p>
       </div>
       <div>
@@ -44,45 +61,44 @@ const BikeList: React.FC = () => {
           </Typography>
         </div>
         <div>
-          {bikes
-            ?.filter((bike) => bike.brand === brand)
-            .map((bike) => (
-              <Card
-                key={bike.id}
-                className="hover:shadow-lg md:flex md:flex-1/2 md:mr-10 md:justify-evenly justify-center"
-                sx={{
-                  borderRadius: 5,
-                  borderWidth: 1,
-                  marginY: 3,
-                  borderColor: "gray",
-                }}
-              >
-                <img
-                  src={bike.image}
-                  alt={bike.name}
-                  className="w-fit h-[200px] object-fit rounded"
-                />
-                <CardContent className="bg-gray-50">
-                  <h2 className="text-md font-semibold">{bike.name}</h2>
-                  <p className="text-bold text-black text-sm mb-2">
-                    {bike.engineCapacity} &bull; {bike.mileageARAI} &bull; {bike.power} &bull; {bike.kerbWeight}
-                  </p>
-                  <p className="text-md font-semibold flex">
-                    ₹ {bike.onRoadPrice} <span className="pl-1">Onwards</span>
-                  </p>
-                  <span className="text-sm mb-2">On-Road Price in Mumbai</span>
-                  <Button
-                    variant="text"
-                    color="primary"
-                    fullWidth
-                    sx={{ border: 1, marginTop: 2, borderRadius: 2 }}
-                    onClick={() => navigate(`checkout/${bike.name}`)}
-                  >
-                    check out
-                  </Button>
-                </CardContent>
-              </Card>
-            ))}
+          {filterByBrand?.map((bike) => (
+            <Card
+              key={bike.id}
+              className="hover:shadow-lg md:flex md:flex-1/2 md:mr-10 md:justify-evenly justify-center"
+              sx={{
+                borderRadius: 5,
+                borderWidth: 1,
+                marginY: 3,
+                borderColor: "gray",
+              }}
+            >
+              <img
+                src={bike.image}
+                alt={bike.name}
+                className="w-fit h-[200px] object-fit rounded"
+              />
+              <CardContent className="bg-gray-50">
+                <h2 className="text-md font-semibold">{bike.name}</h2>
+                <p className="text-bold text-black text-sm mb-2">
+                  {bike.engineCapacity} &bull; {bike.mileageARAI} &bull;{" "}
+                  {bike.power} &bull; {bike.kerbWeight}
+                </p>
+                <p className="text-md font-semibold flex">
+                  ₹ {bike.onRoadPrice} <span className="pl-1">Onwards</span>
+                </p>
+                <span className="text-sm mb-2">On-Road Price in Mumbai</span>
+                <Button
+                  variant="text"
+                  color="primary"
+                  fullWidth
+                  sx={{ border: 1, marginTop: 2, borderRadius: 2 }}
+                  onClick={() => navigate(`checkout/${bike.name}`)}
+                >
+                  check out
+                </Button>
+              </CardContent>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
